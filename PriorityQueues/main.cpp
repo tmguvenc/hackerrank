7c4e3fe0dfabe408a7f69bb722ee5e6d7344577e
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2017-01-12 16:31:20 +0300

    priority queue construction and sorting

--- PriorityQueues/main.cpp
+++ PriorityQueues/main.cpp
@@ -8,12 +8,28 @@ class PriorityQueue{
 public:
     PriorityQueue(int max_size) :
         m_max_size(max_size),
-        N(0)
+        N(0),
+        m_arr(new T[m_max_size])
     {
-        m_arr = new T[m_max_size];
     }
+
+    PriorityQueue(const std::initializer_list<int>& list){
+        using size_type = std::initializer_list<int>::size_type;
+        size_type size = list.size();
+        m_arr = new T[size];
+        N = size-1;
+        m_max_size = size;
+        std::copy(list.begin(), list.end(), m_arr);
+        for(size_type i = size / 2; i > 0; --i){
+            sink(i);
+        }
+    }
+
     ~PriorityQueue(){
-        delete[] m_arr;
+        if(m_arr){
+            delete[] m_arr;
+            m_arr = nullptr;
+        }
     }
 
     void push(const T& item){
@@ -68,39 +84,49 @@ private:
 
 int main(int argc, char *argv[])
 {
-    using MaxPQ = PriorityQueue<char, less<char>>;
-    using MinPQ = PriorityQueue<char, greater<char>>;
-    MaxPQ max_queue(1000);
-    MinPQ min_queue(1000);
+//    using CharMaxPQ = PriorityQueue<char, less<char>>;
+//    using CharMinPQ = PriorityQueue<char, greater<char>>;
+    using IntMinPQ = PriorityQueue<int, greater<int>>;
+//    CharMaxPQ max_queue(1000);
+//    CharMinPQ min_queue(1000);
 
-    stringstream ss;
-    string line;
+//    stringstream ss;
+//    string line;
 
-    while(std::getline(std::cin, line)){
-        ss << line;
-    }
+//    while(std::getline(std::cin, line)){
+//        ss << line;
+//    }
 
-    const auto str = ss.str();
+//    const auto str = ss.str();
 
-    cout << str << endl << endl;
+//    cout << str << endl << endl;
 
-    for(auto c : str){
-        if(c == ' ') continue;
-        max_queue.push(c);
-        min_queue.push(c);
-    }
+//    for(auto c : str){
+//        if(c == ' ') continue;
+//        max_queue.push(c);
+//        min_queue.push(c);
+//    }
 
-    cout << "Descending: \t";
-    while(!max_queue.empty()){
-        cout << max_queue.top();
-        max_queue.pop();
-    }
+//    cout << "Descending: \t";
+//    while(!max_queue.empty()){
+//        cout << max_queue.top();
+//        max_queue.pop();
+//    }
 
-    cout << endl << "Ascending: \t";
+//    cout << endl << "Ascending: \t";
 
-    while(!min_queue.empty()){
-        cout << min_queue.top();
-        min_queue.pop();
+//    while(!min_queue.empty()){
+//        cout << min_queue.top();
+//        min_queue.pop();
+//    }
+
+    cout << endl << "Constructed: \t";
+
+    IntMinPQ queue({3,4,2,3,5,1,43,53,45,1,1,423,5,6,2,9,8,6});
+
+    while(!queue.empty()){
+        cout << queue.top() << " ";
+        queue.pop();
     }
 
     cout << endl;
