0b0ffba542e2ee12d27a598fd7bd0bd572262e4b
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2017-01-16 21:29:42 +0300

    changed recursive function

--- find_minimum_element_in_rotated_array/main.cpp
+++ find_minimum_element_in_rotated_array/main.cpp
@@ -2,24 +2,32 @@
 
 using namespace std;
 
-int find_min(int arr[], int size){
-    if(size < 2)
-        return std::min(arr[0], arr[1]);
+int find_min(int arr[], int left, int right){
+    if(left >= right)
+        return left;
 
-    if(arr[0] > arr[size/2])
-        return find_min(arr, size / 2);
-    else
-        return find_min(arr + size / 2, size - size / 2);
+    int middle = left + (right - left) / 2;
+
+    if(middle < right && arr[middle] > arr[middle + 1])
+        return middle + 1;
+
+    if(middle > left && arr[middle] < arr[middle - 1])
+        return middle;
+
+    if(arr[middle] > arr[right])
+        return find_min(arr, middle + 1, right);
+
+    return find_min(arr, left, middle - 1);
+}
+
+int find_min(int arr[], int size){
+    return find_min(arr, 0, size - 1);
 }
 
 int main(int argc, char *argv[])
 {
-    int arr[] = {  5, 6, 7, 1, 2, 3, 4, };
-
+    int arr[] = {  3, 4, 5, 6, 7, 1, 2, };
     auto len = sizeof(arr) / sizeof(int);
-
-    if(arr[0] > arr[len - 1])
-        cout << find_min(arr, len) <<  endl;
-
+    cout << find_min(arr, len) <<  endl;
     return 0;
 }
