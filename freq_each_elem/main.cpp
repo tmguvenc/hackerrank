41d40e7ffcd47916da06858be22accec00182e0b
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2017-01-16 15:31:59 +0300

    find repeating element deleted

--- freq_each_elem/main.cpp
+++ freq_each_elem/main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
+/*
+ * Find frequency of each element in a limited range array in less than O(n) time.
+ *
+ * Given an sorted array of positive integers, count number of occurrences for each element in the array. Assume all elements in the array are less than some constant M.
+ *
+ * Do this without traversing the complete array. i.e. expected time complexity is less than O(n).
+ *
+ * algorithmic complexity = O(m log n), where m is the count of distinct elements
+ *
+*/
+
+void freq(int arr[], int left, int right, unordered_map<int, int>& s){
+    if(arr[left] == arr[right]){
+        s[arr[left]] += right - left + 1;
+        return;
+    }
+
+    int middle = left + (right - left) / 2;
+
+    freq(arr, left, middle, s);
+    freq(arr, middle + 1, right, s);
+}
+
+void freq(int arr[], int size){
+    unordered_map<int, int> s;
+    freq(arr, 0, size - 1, s);
+    for(auto it = s.begin(); it != s.end(); ++it){
+        cout << it->first << ": " << it->second << endl;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    int arr[] = { 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 5,
+                  5,5,5,5,5, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 10, 10, 10 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    freq(arr, size);
+
     return 0;
 }
--- longest_common_prefix/main.cpp
+++ longest_common_prefix/main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
+string getCommonString(const string& s1, const string& s2){
+
+    int size = s1.size() > s2.size() ? s1.size() : s2.size();
+
+    stringstream ss;
+
+    for(int i = 0; i < size; ++i){
+        if(s1[i] == s2[i])
+            ss << s1[i];
+        else
+            break;
+    }
+
+    return ss.str();
+}
+
+string longest_common_prefix(string arr[], int left, int right){
+    if(left == right)
+        return arr[left];
+    int middle = left + (right - left) / 2;
+    auto l = longest_common_prefix(arr, left, middle);
+    auto r = longest_common_prefix(arr, middle + 1, right);
+    return getCommonString(l, r);
+}
+
+string longest_common_prefix(string arr[], int size){
+    return longest_common_prefix(arr, 0, size - 1);
+}
+
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    string arr[] = {"geeksforgeeks", "geeks", "geek", "geezer"};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << longest_common_prefix(arr, size) << endl;
+
     return 0;
 }
