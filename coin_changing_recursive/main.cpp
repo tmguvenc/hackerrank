0a7edb607daca48987fa3272367fce3bf1bcd4de
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2017-01-21 01:25:07 +0300

    longest_increasing_subsequence deleted

--- coin_changing_recursive/main.cpp
+++ coin_changing_recursive/main.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
+#include <unordered_map>
+#include <limits>
 
 using namespace std;
 
+/*
+ * Coin Changing: minimum number of coins to get total using memoization
+*/
+
+long find_min_coin_count(long coins[], long coinCount, long money, unordered_map<long, long>& cache){
+
+    if(money == 0)
+        return 0;
+
+    auto found = cache.find(money);
+    if(found != cache.end())
+        return found->second;
+
+    auto minValue = std::numeric_limits<long>::max();
+
+    for(int i = 0; i < coinCount; ++i){
+        if(coins[i] > money)
+            continue;
+
+        auto remaining = money - coins[i];
+        auto minimum = find_min_coin_count(coins, coinCount, remaining, cache);
+
+        if(minimum < minValue)
+            minValue = minimum;
+    }
+
+    minValue = (minValue == std::numeric_limits<long>::max() ? minValue : minValue + 1);
+
+    cache[money] = minValue;
+
+    return minValue;
+}
+
+long find_min_coin_count(long coins[], long coinCount, long money){
+    unordered_map<long, long> cache;
+    return find_min_coin_count(coins, coinCount, money, cache);
+}
+
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    long coins[] = {1,2,3, 5, 6, 10, 50, 100, 200};
+    long coinCount = sizeof(coins) / sizeof(coins[0]);
+
+    cout << find_min_coin_count(coins, coinCount, 12345) << endl;
+
     return 0;
 }
--- making_anagrams/main.cpp
+++ making_anagrams/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    cout << "Hello World!" << endl;
+// number of characters in english alphabet
+const int CHAR_COUNT = 26;
+
+int number_needed(string a, string b) {
+
+    char chars[CHAR_COUNT + 1] = {0};
+
+    for(const auto& c : a)
+        chars[c - 'a']++;
+    for(const auto& c : b)
+        chars[c - 'a']--;
+
+    int diff = 0;
+    for(int i = 0; i <= CHAR_COUNT; ++i){
+        diff += std::abs(chars[i]);
+    }
+
+    return diff;
+}
+
+int main(){
+    string a;
+    cin >> a;
+    string b;
+    cin >> b;
+    cout << number_needed(a, b) << endl;
     return 0;
 }
